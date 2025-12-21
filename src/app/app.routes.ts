d7250d7297a72d82f525fb1099cbf96ae3d460ed
Milton <gorechinoda@gmail.com>
2025-12-21 12:00:29 +0400

    lesson-03-routing: Add dedicated pages for About, Skills, Projects, and Contact with routing configuration.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomePage} from './pages/home-page/home-page';
+import {AboutPage} from './pages/about-page/about-page';
+import {SkillsPage} from './pages/skills-page/skills-page';
+import {ProjectsPage} from './pages/projects-page/projects-page';
+import {ContactPage} from './pages/contact-page/contact-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePage },
+  { path: 'about', component: AboutPage },
+  { path: 'skills', component: SkillsPage },
+  { path: 'projects', component: ProjectsPage },
+  { path: 'contact', component: ContactPage },
+];
---
+++ src/app/pages/skills-page/skills-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-page',
+  imports: [],
+  templateUrl: './skills-page.html',
+  styleUrl: './skills-page.scss',
+})
+export class SkillsPage {
+
+}
