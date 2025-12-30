2d7405d908ba114845e2b27de596afa69595d540
miltonthefirst <33031936+miltonthefirst@users.noreply.github.com>
2025-12-30 12:32:44 +0400

    lesson-03-routing: Add dedicated pages for About, Skills, Projects, and Contact with routing configuration. (#2)
    Co-authored-by: Milton <gorechinoda@gmail.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './layout/header/header';
 import {Footer} from './layout/footer/footer';
-import {HomePage} from './pages/home-page/home-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, HomePage],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
